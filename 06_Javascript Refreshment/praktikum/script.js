03ecefa5f14299d215abfc90d4800d99b8cdd84f
mortomulu <mfajrihidayat09@gmail.com>
2024-03-13 05:01:29 +0700

    should to fix some input validation

--- 06_Javascript Refreshment/praktikum/script.js
+++ 06_Javascript Refreshment/praktikum/script.js
@@ -3,11 +3,18 @@ const productName = document.querySelector('#productName')
 const productCategory = document.querySelector('#productCategory')
 const fileInput = document.querySelector('#fileInput')
 const productFresh = document.querySelector('#productFresh')
+const brandNew = document.querySelector('#brandNew')
+const secondHand = document.querySelector('secondHand')
+const refufbished = document.querySelector('#refufbished')
 const additionalDesc = document.querySelector('#additionalDesc')
 const productPrice = document.querySelector('#productPrice')
 const submit = document.querySelector('#submit')
 
 console.log(fileInput)
+console.log(productFresh)
+const desc = additionalDesc.value
+console.log(desc)
+console.log(productPrice.value)
 
 productName.addEventListener("input", function() {
     var inputValue = productName.value;
@@ -19,7 +26,7 @@ productName.addEventListener("input", function() {
     }
 });
 
-submit.addEventListener("click", function(){
+submit.addEventListener("click", function(event){
     event.preventDefault();
     if(productName.value == "" || productPrice.value == "" || productCategory.value === "" || fileInput == ""){
         alert('Please enter a valid Input.')
@@ -36,6 +43,18 @@ form.addEventListener('submit', function(event) {
     } else {
         // Lanjutkan dengan tindakan selanjutnya jika file sudah dipilih
         alert('File selected: ' + fileInput.files[0].name);
+        // Lakukan sesuatu jika form valid, seperti mengirim data ke server
+    }
+});
+
+submit.addEventListener('click', function(event) {
+    event.preventDefault(); // Mencegah form untuk melakukan submit secara default
+
+    if (!brandNew.checked && !secondHand.checked && !refufbished.checked) {
+        alert('Please select a gender.');
+    } else {
+        // Lanjutkan dengan tindakan selanjutnya jika gender sudah dipilih
+        alert('Gender selected: ' + document.querySelector('input[name="gender"]:checked').value);
         // Lakukan sesuatu jika form valid, seperti mengirim data ke server
     }
 });
