28009cef68660305f8bdea8f4a8de39c937628cf
mortomulu <mfajrihidayat09@gmail.com>
2024-03-12 22:16:42 +0700

    add soal prioritas

--- 06_Javascript Refreshment/praktikum/script.js
+++ 06_Javascript Refreshment/praktikum/script.js
@@ -1,2 +1,20 @@
 const form = document.querySelector(".form")
-const productname = document.querySelector('#productname')
+const productName = document.querySelector('#productName')
+const productPrice = document.querySelector('#productPrice')
+const submit = document.querySelector('#submit')
+
+productName.addEventListener("input", function() {
+    var inputValue = productName.value;
+    if (inputValue.length > 10) {
+        alert("Product Name must not exceed 10 characters.");
+    }
+});
+
+submit.addEventListener("click", function(){
+    event.preventDefault();
+    if(productName.value == "" || productPrice.value == ""){
+        alert('Please enter a valid Prouct name.')
+    }else{
+
+    }
+})
