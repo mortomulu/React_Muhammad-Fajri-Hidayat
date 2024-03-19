cdfc850de5ca5e1f710d6bc19386f983e316327f
mortomulu <mfajrihidayat09@gmail.com>
2024-03-20 04:40:21 +0700

    add event handling prioritas1

---
+++ 09_React Event Handling/praktikum/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CreateProduct from './pages/CreateProduct'
+
+function App() {
+  return(
+    <>
+      <CreateProduct/>
+    </>
+  )
+}
+
+export default App
