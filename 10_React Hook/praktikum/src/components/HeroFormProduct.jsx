af208485e558ac6997dd0a103d14c65ed402084a
mortomulu <mfajrihidayat09@gmail.com>
2024-03-26 19:54:32 +0700

    add modal edit, should fix func edit

--- 10_React Hook/praktikum/src/components/HeroFormProduct.jsx
+++ 10_React Hook/praktikum/src/components/HeroFormProduct.jsx
@@ -81,7 +81,7 @@ export default function HeroForm(){
 
 
 
-  // State untuk modal
+  // State untuk modal delete
   const [openModalDelete, setOpenModalDelete] = useState(false);
 
 
@@ -99,6 +99,49 @@ export default function HeroForm(){
     setOpenModalDelete(false)
     const updatedData = tableData.filter((data) => data.id !== id);
     setTableData(updatedData);
+  };
+
+
+
+
+
+  // state untuk modal edit
+  const [openModal, setOpenModal] = useState(false);
+
+  // state untuk menangkap id pada list
+  const [productIdToEdit, setProductIdToEdit] = useState(null);
+
+  //fungsi untuk edit
+  const handleEditButtonClick = (id) => {
+    setProductIdToEdit(id)
+    setOpenModal(true)
+  }
+
+  // Fungsi untuk menangani edit
+  const handleEdit = (id) => {
+    // Cari index data yang akan diedit berdasarkan ID
+    const editedIndex = tableData.find((data) => data.id === id);
+    
+    // Buat salinan baru dari data yang akan diedit
+    const editedData = { ...tableData[editedIndex] };
+
+    // Perbarui nilai data yang akan diedit dengan nilai input yang baru
+    editedData.productName = input.productName;
+    editedData.productCategory = input.productCategory;
+    editedData.productFreshness = input.productFreshness;
+    editedData.productPrice = input.productPrice;
+
+    // Buat salinan baru dari array tableData
+    const updatedData = [...tableData];
+
+    // Ganti data yang akan diedit dengan data yang telah diperbarui
+    updatedData[editedIndex] = editedData;
+
+    // Perbarui tableData dengan data yang telah diperbarui
+    setTableData(updatedData);
+
+    // Tutup modal edit
+    setOpenModal(false);
   };
 
 
@@ -350,6 +393,85 @@ export default function HeroForm(){
                     </div>
                   </Modal.Body>
                 </Modal>
+                <Modal show={openModal} onClose={() => setOpenModal(false)}>
+                  <Modal.Header>Edit Product</Modal.Header>
+                  <Modal.Body>
+                    <div className="space-y-6">
+                      {/* Input untuk nama produk */}
+                      <div className="first-name col-span-4">
+                        <label className="text-base w-full flex flex-row" htmlFor="productName">
+                          Product Name
+                        </label>
+                        <input
+                          type="text"
+                          name="productName"
+                          id="productName"
+                          className="w-full mt-3 border-[#CED4DA] rounded"
+                          value={input.productName}
+                          onChange={handleChangeInput}
+                        />
+                      </div>
+                      {/* Input untuk kategori produk */}
+                      <div className="nationality col-span-3">
+                        <label className="w-full flex flex-row" htmlFor="productCategory">Product Category</label>
+                        <select
+                          id="productCategory"
+                          name="productCategory"
+                          autoComplete="country-name"
+                          className="mt-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                          onChange={handleChangeInput}
+                          value={input.productCategory}
+                        >
+                          <option value="">&nbsp;&nbsp;Choose...</option>
+                          <option value="Indonesia">&nbsp;&nbsp;Indonesia</option>
+                          <option value="Malaysia">&nbsp;&nbsp;Malaysia</option>
+                          <option value="Bekasi">&nbsp;&nbsp;Bekasi</option>
+                        </select>
+                      </div>
+                      {/* Input untuk kesegaran produk */}
+                      <div className="gender col-span-6">
+                        <label className="w-full flex flex-row" htmlFor="productFresh">
+                          Product Freshness
+                        </label>
+                        {options.map((option) => (
+                          <div key={option.id} className="mt-3 w-full flex flex-row">
+                            <input
+                              type="radio"
+                              name="productFreshness"
+                              id={option.value}
+                              value={option.value}
+                              checked={input.productFreshness === option.value}
+                              onChange={handleChangeInput}
+                            />
+                            <label htmlFor={option.value} className="ml-1">
+                              {option.label}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                      {/* Input untuk harga produk */}
+                      <div className="col-span-6">
+                        <label className="w-full flex flex-row" htmlFor="productPrice">Product Price</label>
+                        <input
+                          type="number"
+                          name="productPrice"
+                          id="productPrice"
+                          placeholder="$1"
+                          className="w-full mt-3 border-[#CED4DA] rounded"
+                          onChange={handleChangeInput}
+                          value={input.productPrice}
+                        />
+                      </div>
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    {/* Button untuk menyimpan perubahan */}
+                    <Button onClick={() => handleEdit(productIdToEdit)}>Save</Button>
+                    <Button color="gray" onClick={() => setOpenModal(false)}>
+                      Cancel
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
                 <h2 className="text-2xl font-bold mb-4">Data Produk</h2>
                 <div className="overflow-x-auto">
                   <table className="table-auto border-collapse w-full">
@@ -373,7 +495,7 @@ export default function HeroForm(){
                           <td className="border px-4 py-2">{data.productPrice}</td>
                           <td className="border px-4 py-2 gap-1 flex justify-center">
                             <Button className="px-2 py-1 bg-red-600 text-white text-sm rounded" onClick={() => handleDeleteButtonClick(data.id)}>Delete</Button>
-                            <Button className="px-2 py-1 bg-green-500 text-white text-sm rounded">Edit</Button>
+                            <Button className="px-2 py-1 bg-green-500 text-white text-sm rounded" onClick={() => handleEditButtonClick(data.id)}>Edit</Button>
                           </td>
                         </tr>
                       ))}
