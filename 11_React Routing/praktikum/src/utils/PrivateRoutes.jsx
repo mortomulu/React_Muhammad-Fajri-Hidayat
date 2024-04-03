1de64a84151d02b9a85b58fb42971972563756b4
mortomulu <mfajrihidayat09@gmail.com>
2024-04-03 18:46:02 +0700

    add login page

---
+++ 11_React Routing/praktikum/src/utils/PrivateRoutes.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function PrivateRoutes() {
+  const [auth, setAuth] = useState(false);
+
+  return <>
+  {auth ? <Outlet/> :  <Navigate to= '/login'/>}
+  </>;
+}
