f2303c5c8498766fd947f21d3a32ae92479515b5
mortomulu <mfajrihidayat09@gmail.com>
2024-04-03 18:50:55 +0700

    add routes on app

--- 11_React Routing/praktikum/src/App.jsx
+++ 11_React Routing/praktikum/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { LandingPage } from "./pages/LandingPage";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import { Login } from "./pages/Login";
 
 function App() {
 
   return (
-    <>
-      <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PrivateRoutes/>}>
+          <Route element={<LandingPage/>}/>
+        </Route>
+        <Route element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
